62b677d4735424a16159be7fc9c394ff8a6be42a
Vasanth <vasanthsankar21@gmail.com>
2024-11-21 16:47:58 +0530

    Updated

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ toggleButton.addEventListener('click', () => {
     // Toggle the 'open' class on the dropdown menu
     dropdownMenu.classList.toggle('open');
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+  
+    const options = {
+      threshold: 0.4, // Trigger animation when 20% of the section is visible
+    };
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target); // Stop observing after animation is triggered
+        }
+      });
+    }, options);
+  
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  });
+  
