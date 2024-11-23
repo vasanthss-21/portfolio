5613322b7354a52d65516b6975ce868bddcba393
Vasanth <vasanthsankar21@gmail.com>
2024-11-23 23:18:34 +0530

    Updated

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// Select the toggle button and dropdown menu
 const toggleButton = document.querySelector('.togglebtn');
 const dropdownMenu = document.querySelector('.dropdownmenu');
 
-// Add a click event listener to the toggle button
 toggleButton.addEventListener('click', () => {
-    // Toggle the 'open' class on the dropdown menu
     dropdownMenu.classList.toggle('open');
 });
 document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll("section");
   
     const options = {
-      threshold: 0.3, // Trigger animation when 20% of the section is visible
+      threshold: 0.3,
     };
   
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add("visible");
-          observer.unobserve(entry.target); // Stop observing after animation is triggered
+          observer.unobserve(entry.target);
         }
       });
     }, options);
